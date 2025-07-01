b40a3561e3b6185b102d916a696ac21ab08ec7ab
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-01 17:31:43 +0300

    fix: add custom filter

--- src/components/statistic-chart/HeadingForStatistics.tsx
+++ src/components/statistic-chart/HeadingForStatistics.tsx
@@ -1,33 +1,20 @@
-import type { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import type { Dispatch, SetStateAction } from 'react';
 import { TIME_RANGE } from './diagram.data';
+import { Filter } from '../ui/filter/Filter';
 
 interface Props {
   setTimeRangeValue: Dispatch<SetStateAction<'yearly' | 'monthly'>>;
 }
 
 export function HeadingForStatistics({ setTimeRangeValue }: Props) {
-  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const value = e.target.value;
-
-    if (value === 'yearly' || value === 'monthly') {
-      setTimeRangeValue(value);
-    }
-  };
-
   return (
     <div className="flex items-center justify-between">
       <h2 className="text-2xl">Projects Statistic</h2>
-      <select
-        name="time-range"
-        onChange={handleChange}
-        className="px-3 py-1 border border-neutral-400 rounded-full shadow"
-      >
-        {TIME_RANGE.map((item) => (
-          <option key={item.label} value={item.value}>
-            {item.label}
-          </option>
-        ))}
-      </select>
+      <Filter
+        filterName="time-range"
+        stateChangeFunction={setTimeRangeValue}
+        data={TIME_RANGE}
+      />
     </div>
   );
 }
