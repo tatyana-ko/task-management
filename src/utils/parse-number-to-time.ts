b39bc7812ff3fa2a28b2ae6ad89f3ff2418cf2fe
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-01 16:53:05 +0300

    fix: add parameter check

--- src/utils/parse-number-to-time.ts
+++ src/utils/parse-number-to-time.ts
@@ -1,8 +1,14 @@
 export const parseNumberToTime = (number: number) => {
   if (number < 0) return 'Invalid value';
 
-  const hours = Math.floor(number / 60);
-  const minutes = number % 60;
+  if (number < 1440) {
+    const hours = Math.floor(number / 60);
+    const minutes = number % 60;
 
-  return `${hours}h ${minutes}m`;
+    return `${hours}h ${minutes}m`;
+  }
+
+  const days = Math.floor(number / 60 / 24);
+
+  return `${days} days`;
 };
