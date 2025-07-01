eb12d08e97bad301c4663dd183a4a96966dac2f3
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-01 17:30:46 +0300

    feat: add multipurpose filter

---
+++ src/components/ui/filter/Filter.tsx
@@ -0,0 +1,29 @@
+import type { ChangeEvent, Dispatch, SetStateAction } from 'react';
+
+interface Props<T> {
+  filterName: string;
+  data: Array<{ label: string; value: T }>;
+  stateChangeFunction: Dispatch<SetStateAction<T>>;
+}
+
+export function Filter<T extends string>({ filterName, data, stateChangeFunction }: Props<T>) {
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value as T;
+
+    stateChangeFunction(value);
+  };
+
+  return (
+    <select
+      name={filterName}
+      onChange={handleChange}
+      className="px-3 py-1 border border-neutral-400 rounded-full shadow"
+    >
+      {data.map((item) => (
+        <option key={item.label} value={item.value}>
+          {item.label}
+        </option>
+      ))}
+    </select>
+  );
+}
