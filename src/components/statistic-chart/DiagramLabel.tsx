f5fec586766833dac29fcc09f306087aa6a234fc
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-02 17:59:30 +0300

    style: add main colors to global.css and change them in components

--- src/components/statistic-chart/DiagramLabel.tsx
+++ src/components/statistic-chart/DiagramLabel.tsx
@@ -10,7 +10,7 @@ export function DiagramLabel({ x = 350, y = 0 }: LabelProps) {
         fontSize={14}
         width={100}
         height={22}
-        className="text-center text-white font-medium bg-violet-500 rounded-2xl"
+        className="text-center text-white font-medium bg-primary rounded-2xl"
       >
         <div>38 Projects</div>
       </foreignObject>
--- src/components/statistic-chart/DiagramTooltip.tsx
+++ src/components/statistic-chart/DiagramTooltip.tsx
@@ -7,7 +7,7 @@ export function DiagramTooltip({ active, payload }: Props) {
   if (!active || !payload || payload.length === 0) return null;
 
   return (
-    <div className="px-2 py-1 bg-violet-500 text-white rounded-2xl">
+    <div className="px-2 py-1 bg-primary text-white rounded-2xl">
       {payload[0].value} Projects
     </div>
   );
