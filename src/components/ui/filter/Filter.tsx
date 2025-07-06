ec7e17b0a5abbe141bbfde7f6d86815687cd2cc8
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-06 10:52:08 +0300

    feat: add styles to component

--- src/components/ui/filter/Filter.tsx
+++ src/components/ui/filter/Filter.tsx
@@ -1,12 +1,20 @@
+import { ChevronDown } from 'lucide-react';
 import type { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import styles from './filter.module.css';
 
 interface Props<T> {
   filterName: string;
   data: Array<{ label: string; value: T }>;
   stateChangeFunction: Dispatch<SetStateAction<T>>;
+  value: T;
 }
 
-export function Filter<T extends string>({ filterName, data, stateChangeFunction }: Props<T>) {
+export function Filter<T extends string>({
+  filterName,
+  data,
+  stateChangeFunction,
+  value,
+}: Props<T>) {
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
     const value = e.target.value as T;
 
@@ -14,16 +22,19 @@ export function Filter<T extends string>({ filterName, data, stateChangeFunction
   };
 
   return (
-    <select
-      name={filterName}
-      onChange={handleChange}
-      className="px-3 py-1 border border-light-text-color rounded-full shadow"
-    >
-      {data.map((item) => (
-        <option key={item.label} value={item.value}>
-          {item.label}
-        </option>
-      ))}
-    </select>
+    <div className={styles.wrapper}>
+      <select name={filterName} onChange={handleChange} className={styles.select} value={value}>
+        {data.length === 0 ? (
+          <option>No option</option>
+        ) : (
+          data.map((item) => (
+            <option key={item.label} value={item.value}>
+              {item.label}
+            </option>
+          ))
+        )}
+      </select>
+      <div><ChevronDown className={styles.icon} /></div>
+    </div>
   );
 }
