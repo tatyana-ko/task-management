4e84a4c743bcaf7586ef25dc8e0dc8b35c3cf16c
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-06 10:43:37 +0300

    feat: add types for tasks

---
+++ src/types/task.types.ts
@@ -0,0 +1,21 @@
+import type { IUser } from '@/types/user.types';
+
+export interface ISubTask {
+  id: string;
+  title: string;
+  isCompleted: boolean;
+}
+
+export interface ITask {
+  id: string;
+  title: string;
+ 
+  due: Date;
+  users: IUser[];
+  subtasks: ISubTask[]
+  comments: string[];
+  resources: string[];
+  links: string[];
+}
+
+export type TTaskStatus =  'in progress' | 'not started' | 'completed';
