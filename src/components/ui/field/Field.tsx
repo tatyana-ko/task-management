78d8fe4dc230a46a23967acd31ca840562902f10
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-09 11:04:16 +0300

    feat: add custom Field component

---
+++ src/components/ui/field/Field.tsx
@@ -0,0 +1,28 @@
+import type { InputHTMLAttributes } from 'react';
+import type { UseFormRegisterReturn } from 'react-hook-form';
+import { clsx } from 'clsx';
+
+export interface IFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+  error?: string;
+  registration: UseFormRegisterReturn;
+}
+
+export function Field({ label, error, registration, ...props }: IFieldProps) {
+  return (
+    <>
+      <label>
+        <span className="block mb-2 text-light-text-color font-semibold">{label}</span>
+        <input
+          className={clsx(
+            'w-full px-3 py-2 bg-white border rounded',
+            error ? 'border-error' : 'border-transparent',
+          )}
+          {...registration}
+          {...props}
+        />
+      </label>
+      {error && <span className="block mt-1 text-error">{error}</span>}
+    </>
+  );
+}
