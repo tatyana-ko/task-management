4a642b5631ef3d3c53ccf0e2dd132739ae5056a1
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-09 11:09:15 +0300

    style: update style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,6 @@ import { NO_INDEX_PAGE } from '@/constants/seo.constants';
 import type { Metadata } from 'next';
 import { Dashboard } from './Dashboard';
 import Image from 'next/image';
-import ChatImage from '/chat-img.png';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -13,7 +12,7 @@ export default function DashboardPage() {
   return (
     <div className="grid grid-cols-[75%_25%]">
       <Dashboard />
-      <div className="bg-chat-bg-color text-center text-white h-[100vh]">
+      <div className="bg-chat-bg-color text-center text-white h-[100%]">
         <Image
           src="/chat-img.png"
           alt="chat image"
