80d352ec5ec0015265b96d04e50cde5f434608db
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-09 10:48:05 +0300

    feat: add img for chat

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { NO_INDEX_PAGE } from '@/constants/seo.constants';
 import type { Metadata } from 'next';
 import { Dashboard } from './Dashboard';
+import Image from 'next/image';
+import ChatImage from '/chat-img.png';
 
 export const metadata: Metadata = {
   title: 'Dashboard',
@@ -11,7 +13,17 @@ export default function DashboardPage() {
   return (
     <div className="grid grid-cols-[75%_25%]">
       <Dashboard />
-      <div className="bg-chat-bg-color text-center text-white h-[100vh]">CHAT</div>
+      <div className="bg-chat-bg-color text-center text-white h-[100vh]">
+        <Image
+          src="/chat-img.png"
+          alt="chat image"
+          width={0}
+          height={0}
+          sizes="100vw"  
+          className="w-full h-auto"
+        />
+        <div>CHAT</div>
+      </div>
     </div>
   );
 }
