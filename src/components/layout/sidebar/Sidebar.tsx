d725e6460687b0365f3905b4ef378f8fa197e001
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-13 17:54:50 +0300

    fix: hydration failed

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,12 +1,17 @@
-import { ThemesSwitcher } from '@/components/themes/ThemesSwitcher';
+'use client'
+
+import dynamicNext from "next/dynamic";
 import { Account } from './Account';
 import { MainMenu } from './MainMenu';
 import { Projects } from './Projects';
 
+const DynamicThemesSwitcher = dynamicNext(() => import('@/components/themes/ThemesSwitcher').then((mod) => mod.ThemesSwitcher), {
+	ssr: false})
+
 export function Sidebar() {
   return (
     <aside className="p-4 bg-accent-bg h-full">
-      <ThemesSwitcher />
+      <DynamicThemesSwitcher />
       <Account />
       <MainMenu />
       <Projects />
