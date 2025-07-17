5000afa2fa13cae612cc637bd79e7f0915d96ab4
Tatyana Koba <misskotanyaz@gmail.com>
2025-07-17 16:54:28 +0300

    fix: use getUser instead getSession for safety

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -84,9 +84,9 @@ export async function logout() {
 export async function getUserSession() {
   const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.getSession();
+  const { data, error } = await supabase.auth.getUser();
 
   if (error) return null;
 
-  return { status: 'success', user: data.session?.user };
+  return { status: 'success', user: data?.user };
 }
